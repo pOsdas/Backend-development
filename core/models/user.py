4784d2c1a68955bb2c8d62bbcb0a127cc312418d
pOsdas <ratmir1099@mail.ru>
2024-08-13 14:54:52 +0300

    create relationships between user and post

--- core/models/user.py
+++ core/models/user.py
@@ -1,9 +1,15 @@
+from typing import TYPE_CHECKING
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+from . import Post
 from .base import Base
+
+if TYPE_CHECKING:
+    from .post import Post
 
 
 class User(Base):
     user_name: Mapped[str] = mapped_column(String(32), unique=True)
 
+    posts: Mapped[list["Post"]] = relationship(back_populates="user")
