2bf9d1b920de7be2acdf80f5236b3f461976ef51
pOsdas <ratmir1099@mail.ru>
2024-08-17 01:29:41 +0300

    link product.py and order.py using association table

--- core/models/product.py
+++ core/models/product.py
@@ -1,6 +1,11 @@
+from typing import TYPE_CHECKING
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
 from .base import Base
 
-from sqlalchemy.orm import Mapped, mapped_column
+if TYPE_CHECKING:
+    from .order import Order
+    from .order_product_associations import OrderProductAssociation
 
 
 class Product(Base):
@@ -9,3 +14,13 @@ class Product(Base):
     name: Mapped[str] = mapped_column(nullable=False)
     description: Mapped[str] = mapped_column(nullable=False)
     price: Mapped[int] = mapped_column(nullable=False)
+
+    orders: Mapped[list["Order"]] = relationship(
+        secondary="order_product_association",
+        back_populates="products",
+
+    )
+
+    orders_details: Mapped[list["OrderProductAssociation"]] = relationship(
+        back_populates="product"
+    )
