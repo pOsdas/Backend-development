6407337eda8971e13fbf8a4d2da264570cb415a2
pOsdas <ratmir1099@mail.ru>
2024-08-25 01:28:14 +0300

    add AuthJWT model

--- core/config.py
+++ core/config.py
@@ -13,10 +13,19 @@ class DbSettings(BaseModel):
     # echo: bool = True
 
 
+class AuthJWT(BaseModel):
+    private_key_path: Path = BASE_DIR / "certs" / "jwt-private.pem"
+    public_key_path: Path = BASE_DIR / "certs" / "jwt-public.pem"
+    algorithm: str = "RS256"
+    access_token_expires_in: int = 15  # 3
+
+
 class Settings(BaseSettings):
     api_v1_prefix: str = "/api/v1"
 
     db: DbSettings = DbSettings()
+
+    auth_jwt: AuthJWT = AuthJWT()
 
 
 settings = Settings()
